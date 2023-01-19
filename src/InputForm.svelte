cf33b6fd7e3b238337bf5f21de08d0fa207fe0eb
Ibrah <ibrah@pop-os.localdomain>
2023-01-19 07:19:03 +0300

    added input form onMount State

--- src/InputForm.svelte
+++ src/InputForm.svelte
@@ -1,14 +1,20 @@
 <script>
     import {toDoItems} from './store';
+    import {onMount} from 'svelte'
     export let useName;
-    let newItem = ''    
+    let newItem = ''   
+    let newItemForm = null;
+    
+    onMount (() => {
+        newItemForm.focus()
+    })
     function addToList(){
         $toDoItems = [...$toDoItems, {text: newItem, status:false}]
         newItem = '';
     }
 
     </script>
-    <input bind:value={newItem} type="text" placeholder="Add New Task">
+    <input bind:value={newItem}  bind:this={newItemForm} type="text" placeholder="Add New Task">
     <button on:click={addToList}>Add</button>
     <h3>{useName}'s task List: </h3>
     
