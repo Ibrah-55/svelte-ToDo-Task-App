a3ffd4ff22d5d8c784b3f67104475fece0696a59
Ibrah <ibrah@pop-os.localdomain>
2023-01-20 02:19:30 +0300

    cahnged store status setting

--- src/store.js
+++ src/store.js
@@ -2,9 +2,9 @@ import { writable} from "svelte/store";
 
 
 export const toDoItems = writable([
-    {text: "Take Trash Out", status: true},
+    {text: "Take Trash Out", status: false},
     {text: "Go shopping", status:  false},
-    {text: "Deploy Application", status: true},
+    {text: "Deploy Application", status: false},
     {text: "Go bike Riding", status: false},
 
 ])
